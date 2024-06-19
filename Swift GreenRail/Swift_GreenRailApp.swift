f596479ac4f9f587115f908abf64f8712f1f7be5
Aditya Mhatre <amhatre@ashleyfurniture.com>
2024-06-19 15:24:47 +0530

    Initial Commit

---
+++ Swift GreenRail/Swift_GreenRailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_GreenRailApp.swift
+//  Swift GreenRail
+//
+//  Created by Aditya Mhatre on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_GreenRailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
