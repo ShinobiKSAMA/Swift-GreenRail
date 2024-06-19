7eaf0a04ec1ead5fa3db7967dd181c56b2b20921
Aditya Mhatre <amhatre@ashleyfurniture.com>
2024-06-19 16:46:02 +0530

    Navigation Bar and MyAccount Screen

---
+++ Swift GreenRail/UI/MyAccountScreen.swift
@@ -0,0 +1,69 @@
+//
+//  MyAccountScreen.swift
+//  Swift GreenRail
+//
+//  Created by Aditya Mhatre on 19/06/24.
+//
+
+import SwiftUI
+
+struct MyAccountScreen: View {
+    var body: some View {
+        List(content: {
+            Section(content: {
+                SettingsTile(icon: "person.crop.circle", title: "Adwait Patil", subTitle: "9212346782")
+            }).padding(.vertical, 8)
+            Section(content: {
+                HStack(
+                    alignment: .top,
+                    content: {
+                        VStack(
+                            alignment: .leading,
+                            content: {
+                                Text("\u{20B9}0.0").font(.system(size: 32, weight: Font.Weight.bold))
+                                Text("Wallet balance").font(.subheadline).foregroundColor(.gray)
+                            }
+                        ).padding(.vertical, 16)
+                        Spacer()
+                        Circle()
+                            .frame(width: 24, height: 24, alignment: .topTrailing)
+                    })
+            }).padding(.horizontal, 8)
+            Section(content: {
+                SettingsTile(icon: "lock.shield", title: "Account Settings")
+                SettingsTile(icon: "creditcard", title: "Saved Cards")
+                SettingsTile(icon: "info.circle", title: "About")
+            }).padding(.vertical, 8)
+            Section(content: {
+                Text("Log out").foregroundColor(.red)
+            }).padding(.vertical, 4)
+        })
+    }
+}
+
+#Preview {
+    MyAccountScreen()
+}
+
+struct SettingsTile: View {
+    let icon: String
+    let title: String
+    var subTitle: String? = nil
+    
+    var body: some View {
+        HStack(content: {
+            Image(systemName: icon)
+            VStack(
+                alignment: .leading,
+                content: {
+                    Text(title)
+                    if(subTitle != nil) {
+                        Text(subTitle!).font(.footnote)
+                    }
+                }
+            )
+            Spacer()
+            Image(systemName: "chevron.right").foregroundColor(.gray)
+        })
+    }
+}
---
+++ Swift GreenRail/UI/NavigatorScreen.swift
@@ -0,0 +1,45 @@
+//
+//  NavigatorScreen.swift
+//  Swift GreenRail
+//
+//  Created by Aditya Mhatre on 19/06/24.
+//
+
+import SwiftUI
+
+struct NavigatorScreen: View {
+    @State private var selectedIndex: Int = 0
+    var body: some View {
+        TabView(selection: $selectedIndex) {
+            NavigationItem(title: "Home", icon: "house.fill", content: {
+                Text("Home View")
+            }).tag(0)
+            NavigationItem(title: "My Bookings", icon: "list.bullet.rectangle.fill", content: {
+                Text("My Bookings View")
+            }).tag(1)
+            NavigationItem(title: "Help", icon: "questionmark.bubble.fill", content: {
+                Text("Help View")
+            }).tag(2)
+            NavigationItem(title: "My Account", icon: "person.crop.circle.fill", content: {
+                MyAccountScreen()
+            }).tag(3)
+        }
+    }
+}
+
+#Preview {
+    NavigatorScreen()
+}
+
+struct NavigationItem<Content: View>: View {
+    let title: String
+    let icon: String
+    @ViewBuilder let content: Content
+    
+    var body: some View {
+        NavigationView() {
+            content.navigationTitle(title).toolbarTitleDisplayMode(.inlineLarge)
+        }
+        .tabItem {Label(title, systemImage: icon)}
+    }
+}
